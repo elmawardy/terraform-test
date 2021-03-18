8f06bc8c4716a171b6c057c685440aa7b939bf89
elmawardy <amr.elmawardy@must.edu>
2021-03-19 01:12:13 +0200

    use official module

--- elmawardy.tf
+++ elmawardy.tf
@@ -12,47 +12,70 @@ secret_key = "<your secret key>"
   region = "us-east-1"
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = module.my-eks-cluster.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.my-eks-cluster.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.9"
+}
+
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
   }
 }
 
-provider "kubernetes" {
-  config_path    = "~/.kube/config"
-}
-
-
 module "vpc" {
-  source = "git::ssh://git@github.com/reactiveops/terraform-vpc.git?ref=v5.0.1"
+  source = "terraform-aws-modules/vpc/aws"
+
   name = "${local.cluster_name}-vpc"
+  cidr = "10.0.0.0/16"
 
-  aws_region = "us-east-1"
-  az_count   = 3
-  aws_azs    = "us-east-1a, us-east-1b, us-east-1c"
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  global_tags = {
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+  vpc_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
   }
 }
 
-module "eks" {
-  source       = "git::https://github.com/terraform-aws-modules/terraform-aws-eks.git?ref=v12.1.0"
-  cluster_name = local.cluster_name
-  vpc_id       = module.vpc.aws_vpc_id
-  subnets      = module.vpc.aws_subnet_private_prod_ids
+module "my-eks-cluster" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.17"
+  subnets         = module.vpc.public_subnets
+  vpc_id          = module.vpc.vpc_id
 
-  node_groups = {
-    eks_nodes = {
-      desired_capacity = 3
-      max_capacity     = 3
-      min_capaicty     = 3
-
-      instance_type = "t2.small"
+  worker_groups = [
+    {
+      instance_type = "t2.medium"
+      asg_max_size  = 1
+      root_volume_type = "gp2"
     }
-  }
-
-  manage_aws_auth = false
+  ]
 }
 
 
